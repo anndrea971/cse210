75ef91272b72eb36c3610e29f1ee964fd6b6324f
anndrea971 <agonzalezsommermeyer@byupathway.edu>
2025-11-01 19:06:45 -0300

    excersiscee 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        string sign = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = percent % 10;
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit <= 3)
+        {
+            sign = "-";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        
+        if (letter == "A")
+        {
+            if (sign == "+")
+            {
+                sign = "";
+            }   
+        if (percent >= 100)
+            {
+                sign = "";
+            }
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed! Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
