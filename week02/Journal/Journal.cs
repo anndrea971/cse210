1cc8c26977465f3fdb733aaa3e2dbf796db3873d
anndrea971 <agonzalezsommermeyer@byupathway.edu>
2025-11-08 16:29:39 -0300

    yes

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,14 @@
+// Journal.cs
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+    private const string Separator = "~~~"; 
+
+    // Add all your Journal methods here (AddEntry, DisplayAll, SaveToFile, LoadFromFile)
+    // ...
+}
