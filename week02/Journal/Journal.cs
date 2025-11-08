40f15e89225d151f433a8184f7adfbae10921668
anndrea971 <agonzalezsommermeyer@byupathway.edu>
2025-11-08 16:39:35 -0300

    cambios

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,4 +1,3 @@
-// Journal.cs
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,6 +8,24 @@ public class Journal
     private List<Entry> _entries = new List<Entry>();
     private const string Separator = "~~~"; 
 
-    // Add all your Journal methods here (AddEntry, DisplayAll, SaveToFile, LoadFromFile)
-    // ...
+    // ESTOS MÉTODOS DEBEN SER PÚBLICOS
+    public void AddEntry(Entry newEntry) // <-- Debe ser public
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll() // <-- Debe ser public y con D mayúscula
+    {
+        // ... Lógica para mostrar las entradas
+    }
+
+    public void SaveToFile(string filename) // <-- Debe ser public y con S mayúscula
+    {
+        // ... Lógica para guardar
+    }
+
+    public void LoadFromFile(string filename) // <-- Debe ser public y con L mayúscula
+    {
+        // ... Lógica para cargar
+    }
 }
